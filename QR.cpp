2f16771ff311af03b9689a2ba05cf7c248b2ac49
Junqiang Qian <jq514@ic.ac.uk>
2016-03-09 01:01:23 +0000

    Implementation of useful vector/matrices helper functions

--- QR.cpp
+++ QR.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <cstdlib>
 #include <vector>
+#include <cmath>
+#include <assert.h>
 #include "random.cpp"
 
 using namespace std;
@@ -8,14 +10,22 @@ using namespace std;
 // #define RANDOM 1
 #define MAX_RAND 100
 
-/* Helper functions */
+/* MATRIX HELPER FUNCTIONS */
+void print_matrix(vector<vector<float> > &matrix);
 void init_matrix(vector<vector<float> > &matrix, int dimensions);
-void generate_random_matrix(vector<vector<float> > &matrix, int dimensions);
-void print_matrix(vector<vector<float> > &matrix, int dimensions);
+void generate_symmetric_matrix(vector<vector<float> > &matrix);
 vector<vector<float> > matrix_multiply(vector<vector<float> > &A,
                                        vector<vector<float> > &B);
+vector<vector<float> > matrix_transpose(vector<vector<float> > &matrix);
+/* MATRIX HELPER FUNCTIONS END*/
 
-/* Helper functions end */
+/* VECTOR HELPER FUNCTIONS */
+void print_vector(vector<float> &vec);
+float vector_magnitude(vector<float> &vec);
+float vector_dot_product(vector<float> &vecA, vector<float> &vecB);
+vector<float> vector_add(vector<float> &vecA, vector<float> &vecB);
+vector<float> vector_sub(vector<float> &vecA, vector<float> &vecB);
+/* VECTOR HELPER FUNCTIONS END */
 
 int main(){
   cout << "QR Decomposition" << endl;
@@ -40,18 +50,24 @@ int main(){
 
   if (n == 0) {
     vector<vector<float> > matrix;
-    vector<vector<float> > matrix2;
     init_matrix(matrix, dimensions);
-    init_matrix(matrix2, dimensions);
-    generate_random_matrix(matrix, dimensions);
-    generate_random_matrix(matrix2, dimensions);
-    vector<vector<float> > result = matrix_multiply(matrix, matrix2);
-    print_matrix(matrix, dimensions);
+    generate_symmetric_matrix(matrix);
+    print_matrix(matrix);
     cout << endl;
-    print_matrix(matrix2, dimensions);
-    cout << endl;
-    print_matrix(result, dimensions);
   }
+}
+
+void print_matrix(vector<vector<float> > &matrix) {
+
+  int dimensions = matrix.size();
+
+  for (int i = 0; i < dimensions; i++) {
+    for (int j = 0; j < dimensions; j++) {
+      cout << matrix[i][j] << "\t";
+    }
+    cout << endl;
+  }
+  cout << endl;
 }
 
 void init_matrix(vector<vector<float> > &matrix, int dimensions) {
@@ -64,16 +80,9 @@ void init_matrix(vector<vector<float> > &matrix, int dimensions) {
   }
 }
 
-void print_matrix(vector<vector<float> > &matrix, int dimensions) {
-  for (int i = 0; i < dimensions; i++) {
-    for (int j = 0; j < dimensions; j++) {
-      cout << matrix[i][j] << "\t";
-    }
-    cout << endl;
-  }
-}
+void generate_symmetric_matrix(vector<vector<float> > &matrix) {
 
-void generate_random_matrix(vector<vector<float> > &matrix, int dimensions) {
+  int dimensions = matrix.size();
 
   /* Assign random values to leading diagonal */
   for (int i = 0; i < dimensions; i++){
@@ -90,8 +99,8 @@ void generate_random_matrix(vector<vector<float> > &matrix, int dimensions) {
 
 vector<vector<float> > matrix_multiply(vector<vector<float> > &A,
                                        vector<vector<float> > &B) {
-   vector<vector<float> > result;
-   init_matrix(result, A.size());
+  vector<vector<float> > result;
+  init_matrix(result, A.size());
 
   for (int i = 0; i < A.size(); i++){
     for (int j = 0; j < A[0].size(); j++) {
@@ -101,5 +110,87 @@ vector<vector<float> > matrix_multiply(vector<vector<float> > &A,
     }
   }
   return result;
+}
 
+vector<vector<float> > matrix_transpose(vector<vector<float> > &matrix) {
+
+  vector<vector<float> > transpose;
+  int dimensions = matrix.size();
+  init_matrix(transpose, dimensions);
+
+  /* Leading diagonal is preserved */
+  for (int i = 0; i < dimensions; i++){
+    transpose[i][i] = matrix[i][i];
+  }
+
+  /* Swap remaining elements with the elements in "mirrored" position with
+     respect to the leading diagonal */
+  for (int i = 0; i < dimensions / 2 + 1; i++) {
+    for (int j = i + 1; j < dimensions; j++) {
+      transpose[i][j] = matrix[j][i];
+      transpose[j][i] = matrix[i][j];
+    }
+  }
+
+  return transpose;
+}
+
+
+void print_vector(vector<float> &vec) {
+  cout << "(";
+  for (int i = 0; i < vec.size(); i++) {
+    i != vec.size() - 1? cout << vec[i] << ", " : cout << vec[i];
+  }
+  cout << ")" << endl;
+}
+
+
+float vector_magnitude(vector<float> &vec) {
+  int dimensions = vec.size();
+  float norm;
+
+  for (int i = 0; i < dimensions; i++) {
+    norm += vec[i] * vec[i];
+  }
+
+  return sqrt(norm);
+}
+
+float vector_dot_product(vector<float> &vecA, vector<float> &vecB) {
+  assert (vecA.size() == vecB.size());
+
+  int dimensions = vecA.size();
+  float dp;
+
+  for (int i = 0; i < dimensions; i++) {
+    dp += vecA[i] * vecB[i];
+  }
+
+  return dp;
+}
+
+vector<float> vector_add(vector<float> &vecA, vector<float> &vecB) {
+  assert (vecA.size() == vecB.size());
+
+  vector<float> result;
+  int dimensions = vecA.size();
+
+  for (int i = 0; i < dimensions; i++) {
+    result.push_back(vecA[i] + vecB[i]);
+  }
+
+  return result;
+}
+
+vector<float> vector_sub(vector<float> &vecA, vector<float> &vecB) {
+  assert (vecA.size() == vecB.size());
+
+  vector<float> negatedVecB;
+  int dimensions = vecA.size();
+
+  for (int i = 0; i < dimensions; i++) {
+    negatedVecB.push_back(-1 * vecB[i]);
+  }
+
+  return vector_add(vecA, negatedVecB);
 }
